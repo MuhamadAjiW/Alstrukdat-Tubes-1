8cfdc252c5eaaeccbb2e82b144080f5cf873c150
AghnaAbyan <aghnamuhammad@gmail.com>
2022-11-19 21:57:36 +0700

    Minor fixes

--- lib/waktu/mekanismewaktu.c
+++ lib/waktu/mekanismewaktu.c
@@ -17,67 +17,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void inventoryDeliveryMechanism(Simulator *S, Stack* stack) {
+void inventoryDeliveryMechanism(Simulator *S, Stack* stack)
+{
     makanan dump, temp;
-    while(timeIsZero(DLV(*S), 2)) {//delivery
+    while(timeIsZero(DLV(*S), 2))
+    {   // Delivery
         Dequeue(&DLV(*S), &temp, 2);
         Enqueue(&INV(*S), temp, 1);
-        //add notif
+        // Add notif
         insertFirst(&NOTIF(*S), 'd', temp);
-        if (Top(*stack) != -1){
+        if (Top(*stack) != -1)
+        {
             insertFirst(&NotifPrev(*stack), '6', temp);
         }
     }
-    while(timeIsZero(INV(*S), 1)) {//expiration
+    while(timeIsZero(INV(*S), 1))
+    {   // Expiration
         Dequeue(&INV(*S), &dump, 1);
-        //add notiff
+        // Add notiff
         insertFirst(&NOTIF(*S), 'e', dump);
-        if (Top(*stack) != -1){
+        if (Top(*stack) != -1)
+        {
             insertFirst(&NotifPrev(*stack), '7', dump);
         }
     }
 }
 
-void passTime(Simulator *S, long pass, waktu *time){
-// melewatkan waktu selama menit yang ditentukan
-// makanan dengan waktu penyimpanan <0 artinya sudah expired
+void passTime(Simulator *S, long pass, waktu *time)
+{
+    // Melewatkan waktu selama menit yang ditentukan
+    // Makanan dengan waktu penyimpanan < 0 artinya sudah expired
     addressQ idx;
     long temp = TIMEToMenit(*time);
     *time = MenitToTIME(temp + pass);
 
-    if(!queueIsEmpty(INV(*S))){
+    if(!queueIsEmpty(INV(*S)))
+    {
         long menit;
-        idx=Head(INV(*S));
-        while(idx!=Tail(INV(*S))){
-            menit=TIMEToMenit(Elmt(INV(*S),idx).expireTime);
-            menit-=pass;
-            Elmt(INV(*S),idx).expireTime=MenitToTIME(menit);
-            if(idx==MaxEl(INV(*S))-1){
-                idx=0;
+        idx = Head(INV(*S));
+        while(idx != Tail(INV(*S)))
+        {
+            menit = TIMEToMenit(Elmt(INV(*S),idx).expireTime);
+            menit -= pass;
+            Elmt(INV(*S),idx).expireTime = MenitToTIME(menit);
+            if(idx == MaxEl(INV(*S))-1)
+            {
+                idx = 0;
             }
             else idx++;
         }
-        menit=TIMEToMenit(InfoTail(INV(*S)).expireTime);
-        menit-=pass;
-        InfoTail(INV(*S)).expireTime=MenitToTIME(menit);
+        menit = TIMEToMenit(InfoTail(INV(*S)).expireTime);
+        menit -= pass;
+        InfoTail(INV(*S)).expireTime = MenitToTIME(menit);
     }
 
-    if (!queueIsEmpty(DLV(*S))) {
+    if (!queueIsEmpty(DLV(*S)))
+    {
         long tMenit;
         int nDelivery = NBElmt(DLV(*S));
         int i = Head(DLV(*S));
-        for (int j = 0; j < nDelivery; j++) {
+        for (int j = 0; j < nDelivery; j++)
+        {
             tMenit = TIMEToMenit(Deliver(DLV(*S), i));
-            if ((tMenit-pass) <= 0) {
+            if ((tMenit-pass) <= 0)
+            {
                 long tempExp = TIMEToMenit(Expire(DLV(*S), i)) + tMenit - pass;
                 Expire(DLV(*S), i) = MenitToTIME(tempExp);
                 Deliver(DLV(*S), i) = MenitToTIME(0);
             }
-            else {
+            else
+            {
                 Deliver(DLV(*S), i) = MenitToTIME(tMenit-pass);
             }
 
-            if (i == MaxEl(DLV(*S))-1) {
+            if (i == MaxEl(DLV(*S))-1)
+            {
                 i = 0;
             }
             else {
--- lib/waktu/waktu.c
+++ lib/waktu/waktu.c
@@ -5,96 +5,113 @@ None
 #include <stdio.h>
 #include <stdlib.h>
 
-void createTime(waktu *t){
+void createTime(waktu *t)
+{
     Hari(*t) = 0;
     Jam(*t) = 0;
     Menit(*t) = 0;
 }
-//inisiasi time
-//isinya 0 0 0
+// Inisiasi time
+// 0 0 0
 
-void printTime(waktu t){
+void printTime(waktu t)
+{
     printf("%d ", Hari(t));
     printf("%d ", Jam(t));
     printf("%d", Menit(t));
 }
-//print time
-//format 0 0 0, tanpa enter
+// Print time
+// Format 0 0 0, tanpa enter
 
-boolean IsTIMEValid (int D, int H, int M) {
-//dipake di createtime
-    if (D >= 0 && H >= 0 && H <= 23 && M >= 0 && M <= 59) {
+boolean IsTIMEValid (int D, int H, int M)
+{
+    // Dipakai di createTime
+    if (D >= 0 && H >= 0 && H <= 23 && M >= 0 && M <= 59)
+    {
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
 }
 
-void CreateTimeDirect (waktu * T, int DD, int HH, int MM) {
-//membuat time dengan input manual
+void CreateTimeDirect (waktu * T, int DD, int HH, int MM) 
+{
+    // Membuat time dengan input manual
     Hari(*T) = DD;
     Jam(*T) = HH;
     Menit(*T) = MM;
-
 }
 
-long TIMEToMenit (waktu T) {
-//dipake di TLT & TGT
-    return (24*60*Hari(T) + 60*Jam(T) + Menit(T));
+long TIMEToMenit (waktu T) 
+{
+    // Dipakai di TLT & TGT
+    return (24 * 60 * Hari(T) + 60 * Jam(T) + Menit(T));
 }
 
-waktu MenitToTIME (long N) {
-//mengubah nilai dari menit kembali ke time
+waktu MenitToTIME (long N)
+{
+    // Mengubah nilai dari menit kembali ke time
     waktu time;
     int N1, hr, min;
-    N1 = N / (24*60);
-    hr = (N % (24*60)) / 60;
-    min = (N % (24*60)) % 60;
+    N1 = N / (24 * 60);
+    hr = (N % (24 * 60)) / 60;
+    min = (N % (24 * 60)) % 60;
     CreateTimeDirect(&time, N1, hr, min);
     return time;
 } 
 
-boolean TLT (waktu T1, waktu T2) {
-/* Mengirimkan true jika T1<T2, false jika tidak */
-    if (TIMEToMenit(T1) < TIMEToMenit(T2)) {
+boolean TLT (waktu T1, waktu T2) 
+{
+    // True jika T1 < T2, false jika tidak
+    if (TIMEToMenit(T1) < TIMEToMenit(T2)) 
+    {
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
 }
 
-boolean TGT (waktu T1, waktu T2){
-/* Mengirimkan true jika T1>T2, false jika tidak */
-    if (TIMEToMenit(T1) > TIMEToMenit(T2)) {
+boolean TGT (waktu T1, waktu T2)
+{
+    // True jika T1 > T2, false jika tidak
+    if (TIMEToMenit(T1) > TIMEToMenit(T2)) 
+    {
         return true;
     }
-    else {
+    else 
+    {
         return false;
     }
 }
 
-void writeHHMM(waktu t){
-//Menuliskan waktu dalam format HH.MM
+void writeHHMM(waktu t)
+{
+    // Menuliskan waktu dalam format HH.MM
     printf("%d.", Jam(t));
     printf("%d", Menit(t));
 }
 
-void nextMinute(waktu * T){
-// mengubah T menjadi waktu setelah 1 menit
-    long menit=TIMEToMenit(*T);
-    *T=MenitToTIME(menit+1);
+void nextMinute(waktu * T)
+{
+    // Mengubah T menjadi waktu setelah 1 menit
+    long menit = TIMEToMenit(*T);
+    *T = MenitToTIME(menit + 1);
 }
 
-void nextTime(waktu * T, int givenMinute){
-// mengubah T menjadi waktu setelah (givenMinute) menit
-    long menit=TIMEToMenit(*T);
-    *T=MenitToTIME(menit+givenMinute);
+void nextTime(waktu * T, int givenMinute)
+{
+    // Mengubah T menjadi waktu setelah (givenMinute) menit
+    long menit = TIMEToMenit(*T);
+    *T = MenitToTIME(menit + givenMinute);
 }
 
-void copyTime(waktu input, waktu *output){
-// menyalin waktu di tempat lain
+void copyTime(waktu input, waktu *output)
+{
+    // Menyalin waktu di tempat lain
     Hari(*output) = Hari(input);
     Jam(*output) = Jam(input);
     Menit(*output) = Menit(input);
